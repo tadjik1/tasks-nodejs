a263111dba8462bfd92205c4279319682d9a92e7
Sergey Zelenov <mail@zelenov.su>
2019-04-19 15:36:11 +0200

    remove logger

--- 5-module/1-task/solution/app.js
+++ 5-module/1-task/solution/app.js
@@ -2,7 +2,6 @@ const Koa = require('koa');
 const app = new Koa();
 
 app.use(require('koa-static')('public'));
-app.use(require('koa-logger')());
 app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
